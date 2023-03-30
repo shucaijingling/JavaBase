48444bf8169fdd10e55ddaa7819001c259ed8b7d
x <shucai@>
2023-03-30 21:05:25 +0800

    add 1.1

--- src/main/java/com/async/AsyncExample2.java
+++ src/main/java/com/async/AsyncExample2.java
@@ -1,45 +1,40 @@
 package com.async;
 
-import com.shucai.async.annotation.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
-@ThreadSafe
 @Slf4j
-public class AsyncExample {
+public class AsyncExample2 {
 
-    private static int testTotal = 5000;
-    private static int clientTotal = 200;
-    private static int count = 0;
-
-    public static void main(String[] args) throws Exception {
+    private static AtomicInteger count = new AtomicInteger(0);
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        final Semaphore semaphore = new Semaphore(clientTotal);
-        final CountDownLatch countDownLatch = new CountDownLatch(testTotal);
-        for (int i = 0; i < testTotal; i++) {
+        final Semaphore semaphore = new Semaphore(200);
+        final CountDownLatch countDownLatch = new CountDownLatch(10000);
+        for (int i = 0; i < 10000; i++) {
             executorService.execute(() -> {
                 try {
                     semaphore.acquire();
                     add();
                     semaphore.release();
                 } catch (InterruptedException e) {
-                    log.error("error : {}", count);
+                    throw new RuntimeException(e);
                 }
                 countDownLatch.countDown();
             });
-
         }
         countDownLatch.await();
         executorService.shutdown();
-        log.info("count:{}", count);
+        log.info("success : {}", count);
     }
 
     public static void add() {
-        count++;
+        count.incrementAndGet();
         log.info("count : {}", count);
     }
 }
