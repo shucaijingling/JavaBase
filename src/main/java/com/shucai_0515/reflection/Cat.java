68c4f567bc261c2cdf1856844f6bcb151359f9e8
shucaijingling <A421750169@126.com>
2023-05-16 20:58:58 +0800

    reflection-2

--- src/main/java/com/shucai_0515/reflection/Cat.java
+++ src/main/java/com/shucai_0515/reflection/Cat.java
@@ -4,7 +4,21 @@ public class Cat {
 
     private String name = "招财猫";
 
+    public int age = 10;
+
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
     public void hi() {
-        System.out.println("hi " + name);
+//        System.out.println("hi " + name);
+    }
+
+
+    public void cry() {
+        System.out.println("cry " + name);
     }
 }
---
+++ src/main/java/com/shucai_0515/reflection/Reflection02.java
@@ -0,0 +1,55 @@
+package com.shucai_0515.reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * 直接调用和反射调用性能比较
+ */
+@SuppressWarnings({"all"})
+public class Reflection02 {
+
+    public static void main(String[] args) throws Exception{
+
+        m1();
+        m2();
+        m3();
+    }
+    //直接调用
+    public static void m1() {
+        Cat c = new Cat();
+        long strat = System.currentTimeMillis();
+        for (int i = 0; i < 900000000; i++) {
+            c.hi();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("m1耗时： " + (end - strat));
+    }
+    //反射调用
+    public static void m2() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class<?> cls = Class.forName("com.shucai_0515.reflection.Cat");
+        Object o = cls.newInstance();
+        Method hi = cls.getMethod("hi");
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 900000000; i++) {
+            hi.invoke(o);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("m2耗时 ： " + (end - start));
+    }
+
+    //反射优化，取消访问检查
+    public static void m3() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class<?> cls = Class.forName("com.shucai_0515.reflection.Cat");
+        Object o = cls.newInstance();
+        Method hi = cls.getMethod("hi");
+        hi.setAccessible(true);
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 900000000; i++) {
+            hi.invoke(o);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("m2耗时 ： " + (end - start));
+    }
+
+}
